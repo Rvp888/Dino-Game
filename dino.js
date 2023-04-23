223bd6abdc6a84e0ca73a4c09de1048dd4cc043e
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-23 23:41:25 +0530

    files modified

--- dino.js
+++ dino.js
@@ -1,22 +1,36 @@
+import { getCustomProperty, incrementCustomProperty, setCustomProperty } from "./updateCustomProperty.js";
 
 const dinoElem = document.querySelector("[data-dino]");
 const JUMP_SPEED = 0.45;
-const GRAVITY = 0.011;
+const GRAVITY = 0.0015;
 const DINO_FRAME_COUNT = 2;
 const FRAME_TIME = 100;
 
 let isJumping;
 let dinoFrame;
 let currentFrameTime;
+let yVelocity;
 export function setupDino() {
     isJumping = false;
     dinoFrame = 0;
     currentFrameTime = 0;
+    yVelocity = 0;
+    setCustomProperty(dinoElem, "--bottom", 0)
+    document.removeEventListener("keydown", onJump);
+    document.addEventListener("keydown", onJump);
 }
 
 export function updateDino(delta, speedScale) {
     handleRun(delta, speedScale);
-    handleJump();
+    handleJump(delta);
+}
+
+export function getDinoRect() {
+    return dinoElem.getBoundingClientRect();
+}
+
+export function setDinoLose() {
+    dinoElem.src = "images/dino-lose.png";
 }
 
 function handleRun(delta, speedScale) {
@@ -33,6 +47,22 @@ function handleRun(delta, speedScale) {
     currentFrameTime += delta * speedScale;
 }
 
-function handleJump() {
-    
+function handleJump(delta) {
+    if (!isJumping) return;
+
+    incrementCustomProperty(dinoElem, "--bottom", yVelocity * delta);
+
+    if (getCustomProperty(dinoElem, "--bottom") <= 0) {
+        setCustomProperty(dinoElem, "--bottom", 0);
+        isJumping = false;
+    }
+
+    yVelocity -= GRAVITY * delta;
+}
+
+function onJump(e) {
+    if (e.code !== "Space" || isJumping) return;
+
+    yVelocity = JUMP_SPEED;
+    isJumping = true;
 }
